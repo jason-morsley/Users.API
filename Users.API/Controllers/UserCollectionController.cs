f396b95656198211cc8b9812d3cef014467c7ce5
Jason Morsley <GitHub@jasonmorsley.dev>
2020-11-23 00:05:03 +0000

    Support for HttpPost using a collection resource

---
+++ Users.API/Controllers/UserCollectionController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
+using Users.API.Helpers;
+using Users.API.Models;
+using Users.API.Services;
+
+namespace Users.API.Controllers
+{
+    [ApiController]
+    [Route("api/usercollections")]
+
+    public class UserCollectionController : ControllerBase
+    {
+        private readonly IUserRepository _userRepository;
+        private readonly IMapper _mapper;
+
+        public UserCollectionController(IUserRepository userRepository,
+            IMapper mapper)
+        {
+            _userRepository = userRepository ?? throw new ArgumentNullException(nameof(userRepository));
+            _mapper = mapper ?? throw new ArgumentNullException(nameof(mapper));
+        }
+
+        [HttpGet("({ids})", Name = "GetUserCollection")]
+        public IActionResult GetUserCollection([FromRoute][ModelBinder(binderType: typeof(ArrayModelBinder))] IEnumerable<Guid> ids)
+        {
+            if (ids == null)
+            {
+                return BadRequest();
+            }
+
+            var userEntities = _userRepository.GetUsers(ids);
+
+            if (ids.Count() != userEntities.Count())
+            {
+                return NotFound();
+            }
+
+            var usersToReturn = _mapper.Map<IEnumerable<UserDto>>(userEntities);
+
+            return Ok(usersToReturn);
+        }
+
+        [HttpPost]
+        public ActionResult<IEnumerable<UserDto>> CreateUserCollection(IEnumerable<UserForCreationDto> userCollection)
+        {
+            var userEntities = _mapper.Map<IEnumerable<Entities.User>>(userCollection);
+            foreach (var user in userEntities)
+            {
+                _userRepository.AddUser(user);
+            }
+
+            _userRepository.Save();
+
+            var userCollectionToReturn = _mapper.Map<IEnumerable<UserDto>>(userEntities);
+            var idsAsString = string.Join(",", userCollectionToReturn.Select(a => a.Id));
+            return CreatedAtRoute("GetUserCollection", new {ids = idsAsString}, userCollectionToReturn);
+        }
+    }
+}
---
+++ Users.API/Helpers/ArrayModelBinder.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Reflection;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding;
+
+namespace Users.API.Helpers
+{
+    // IModelBinder: https://docs.microsoft.com/en-us/dotnet/api/system.web.mvc.imodelbinder?view=aspnet-mvc-5.2
+    // BindModelAsync: https://docs.microsoft.com/en-us/dotnet/api/microsoft.aspnetcore.mvc.modelbinding.imodelbinder.bindmodelasync?view=aspnetcore-5.0
+    public class ArrayModelBinder : IModelBinder
+    {
+        public Task BindModelAsync(ModelBindingContext bindingContext)
+        {
+            // Check if non-enumerable, binder works with enumerable types.
+            if (!bindingContext.ModelMetadata.IsEnumerableType)
+            {
+                bindingContext.Result = ModelBindingResult.Failed();
+                return Task.CompletedTask;
+            }
+
+            var value = bindingContext.ValueProvider.GetValue(bindingContext.ModelName).ToString();
+
+            if (string.IsNullOrWhiteSpace(value))
+            {
+                bindingContext.Result = ModelBindingResult.Success(null);
+                return Task.CompletedTask;
+            }
+
+            // If value isn't null, model is enumerable then get type and convert
+            var elementType = bindingContext.ModelType.GetTypeInfo().GenericTypeArguments[0]; // GenericTypeArguments for us is GUID,
+            var converter = TypeDescriptor.GetConverter(elementType);                         // Create converter for GUID
+
+            // Convert value list to enumerable type
+            var values = value.Split(new[] {","}, StringSplitOptions.RemoveEmptyEntries) 
+                .Select(x => converter.ConvertFromString(x.Trim())).ToArray(); // GUID string to GUID
+
+            // Create array of type, set as model value
+            var typedValues = Array.CreateInstance(elementType, values.Length); // Create array of GUID
+            values.CopyTo(typedValues, 0); // Copy new values to new array
+            bindingContext.Model = typedValues; // Set model on binding context to typed values, model is now an array of GUID
+
+            bindingContext.Result = ModelBindingResult.Success(bindingContext.Model);
+            return Task.CompletedTask;
+        }
+    }
+}
